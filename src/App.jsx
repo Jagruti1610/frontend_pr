1af0b447590e0bea2983689cfee9138aa83e4b25
Jagruti1610 <jahire932@gmail.com>
2026-07-09 17:32:29 +0530

    updated url

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const API_BASE = 'http://127.0.0.1:8000/api/products';
+const API_BASE = `${process.env.REACT_APP_API_URL}/api/products`;
 
 function App() {
   // ---------- STATE ----------
